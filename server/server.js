09d5e68a4fd2779b4b3577e253605d4b8eb026ba
atulim <atul.kaushikim@gmail.com>
2019-09-23 13:02:37 +0530

    Update driver

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname,'../public');
+//console.log(__dirname + '/../public');
+console.log(publicPath);
+
+var app = express();
+app.use(express.static(publicPath));
+
+app.get('/',(req,res) => {
+  res.render('index.html')
+});
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
