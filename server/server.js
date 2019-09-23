9cb303b18889d3c921f665d5d761f2b4b068f5f1
atulim <atul.kaushikim@gmail.com>
2019-09-23 21:18:41 +0530

    Added chat feature

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const publicPath = path.join(__dirname,'../public');
 const socketIO = require('socket.io');
 //console.log(__dirname + '/../public');
 //console.log(publicPath);
-
+const {generateMessage} =require('./utility/message');
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -15,27 +15,27 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket) =>{
   console.log('Hello! New User');
+  socket.emit('newMessage', generateMessage('Hello', 'Whats the issue?'));
+
  //socket.emit('newMsg',{
 //   from:"Atul",
 //   text:"Can we help you?",
 //   createAt: "23rd September"
 
  //});
-
+ socket.broadcast.emit('newMessage', generateMessage('Hello', 'New User'));
 socket.on('createMsg',(message) =>{
   console.log('createMsg', message);
-  io.emit('newMessage', {
-    from: message.from,
-    text: message.text,
-    createdAt:new Date().getTime()
+
+   io.emit('newMessage', generateMessage(message.from, message.text));
+
   });
-});
+
 
   socket.on('disconnect', () => {
     console.log("Disconnected from client");
-  });
 });
-
+});
 //app.get('/',(req,res) => {
 //  res.render('index.html')
 //});
